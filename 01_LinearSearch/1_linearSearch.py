19ea8538396d248c80a363d97d73b3835e02c9c0
Nazeem Khan <nazeemkhanpk@gmail.com>
2023-11-17 00:44:26 +0500

    first commit

---
+++ 01_LinearSearch/1_linearSearch.py
@@ -0,0 +1,25 @@
+# this function will take two arguments 
+# If the target is in the list then we return it's position.
+# The linear search is a sequential search 
+
+def linear_search(list,target):
+    '''Take Two Arguments (List) and (target)'''
+    for i in range(len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print('Target found at index NO: ',index)
+    else:
+        print("Target Not found.")
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+result = linear_search(numbers,10)
+verify(result)
+
+
+
