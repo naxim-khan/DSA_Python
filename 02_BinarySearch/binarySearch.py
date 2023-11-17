99d2a0d936d7a5b529473b5b57ea55ac624565fb
Nazeem Khan <nazeemkhanpk@gmail.com>
2023-11-17 23:07:00 +0500

    second commit

---
+++ 02_BinarySearch/binarySearch.py
@@ -0,0 +1,27 @@
+def binary_search(list,target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first+last)//2  
+        
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+
+
+
+def verify(index):
+    if index is not None:
+        print('Target found at index NO: ',index)
+    else:
+        print("Target Not found.")
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+result = binary_search(numbers,10)
+verify(result)
