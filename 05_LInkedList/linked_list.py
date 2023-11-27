f27110fddfd375ca6ce4d486b3304ef8c3906459
Nazeem Khan <nazeemkhanpk@gmail.com>
2023-11-27 20:53:44 +0500

    linkedlist

--- 05_LInkedList/linked_list.py
+++ 05_LInkedList/linked_list.py
@@ -19,5 +19,20 @@ class Node:
 # print('n1 next node: ', N1.next_node)
 
 class LinkedList:
+
     def __init__(self):
         self.head = None
+
+    def is_emtpy(self):
+        return self.head == None
+    
+    def size(self):
+        '''Return the numbers of nodes in the list takes O(n) time'''
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
